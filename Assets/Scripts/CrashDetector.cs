1c6abe0ab7c571e6a389585eb5642ee6cf6765eb
Muzaffer Mert Akkan <m.mert.akkan@gmail.com>
2024-08-05 17:48:14 +0300

    Sound Effect added

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -11,11 +11,15 @@ public class CrashDetector : MonoBehaviour
     [SerializeField]
     private ParticleSystem crashEffect;
 
+    [SerializeField]
+    private AudioClip crashSFX;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", loadDelay);
         }
     }
---
+++ Assets/Scripts/SnowTrail.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowTrail : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem snowParticles;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            snowParticles.Play();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            snowParticles.Stop();
+        }
+    }
+}
