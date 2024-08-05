19dafb56e695378a2a633728bb4adc8b2293e1ab
Muzaffer Mert Akkan <m.mert.akkan@gmail.com>
2024-08-05 17:21:01 +0300

    Booster functionality

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,14 +10,41 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float torqueAmount = 1f;
 
+    [SerializeField]
+    private float boostSpeed = 35f;
+
+    [SerializeField]
+    private float baseSpeed = 25f;
+
+    SurfaceEffector2D surfaceEffector2D;
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
